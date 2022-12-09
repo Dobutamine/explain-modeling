b710d40aa325d57f706382935eb50c8876454fb8
dobutamine <tim.antonius@outlook.com>
2022-12-09 01:07:16 +0100

    added gasexchangers

--- explain_core/core_models/GasExchanger.py
+++ explain_core/core_models/GasExchanger.py
@@ -8,19 +8,50 @@ class GasExchanger(ModelBaseClass):
     DifCo2 = 0.01
 
     # local parameters
-
+    _flux_o2 = 0.0
+    _flux_co2 = 0.0
 
     def CalcModel(self):
-        # we need to get the To2 and the Po2 of the blood and gas compartment
+        # get the total oxygen and carbon dioxide content from the blood components
         to2_blood = self._modelEngine.Models[self.CompBlood].Solutes["To2"]
         tco2_blood = self._modelEngine.Models[self.CompBlood].Solutes["Tco2"]
 
-        to2_gas = self._modelEngine.Models[self.CompGas].Co2
-        tco2_gas = self._modelEngine.Models[self.CompGas].Cco2
-
-        # calculate the po2 and pco2 of the blood compartment
+        # calculate the partial pressures of oxygen and carbon dioxide from the total content
         po2_blood = self._modelEngine.Models["Oxygenation"].calc_oxygenation(to2_blood).Po2
         pco2_blood = self._modelEngine.Models["AcidBase"].calc_acid_base(tco2_blood).Pco2
 
+        # get the partial pressures from the gas components
+        co2_gas = self._modelEngine.Models[self.CompGas].Co2
+        cco2_gas = self._modelEngine.Models[self.CompGas].Cco2
+        po2_gas = self._modelEngine.Models[self.CompGas].Po2
+        pco2_gas = self._modelEngine.Models[self.CompGas].Pco2
 
+        # calculate the O2 flux from the blood to the gas compartment
+        self._flux_o2 = (po2_blood - po2_gas) * self.DifO2 * self._t
 
+        # calculate the CO2 flux from the blood to the gas compartment
+        self._flux_co2 = (pco2_blood - pco2_gas) * self.DifCo2 * self._t
+
+        # calculate the new O2 concentrations of the gas and blood compartments
+        new_to2_blood = (to2_blood * self._modelEngine.Models[self.CompBlood].Vol - self._flux_o2) / self._modelEngine.Models[self.CompBlood].Vol
+        if (new_to2_blood < 0):
+            new_to2_blood = 0
+
+        new_co2_gas = (co2_gas * self._modelEngine.Models[self.CompGas].Vol + self._flux_o2) / self._modelEngine.Models[self.CompGas].Vol
+        if (new_co2_gas < 0):
+            new_co2_gas = 0
+
+        # calculate the new Co2 concentrations of the gas and blood compartments
+        new_tco2_blood = (tco2_blood * self._modelEngine.Models[self.CompBlood].Vol - self._flux_co2) / self._modelEngine.Models[self.CompBlood].Vol
+        if (new_tco2_blood < 0):
+            new_tco2_blood = 0
+
+        new_cco2_gas = (cco2_gas * self._modelEngine.Models[self.CompGas].Vol + self._flux_co2) / self._modelEngine.Models[self.CompGas].Vol
+        if (new_cco2_gas < 0):
+            new_cco2_gas = 0
+
+        # transfer the new concentrations
+        self._modelEngine.Models[self.CompBlood].Solutes["To2"] = new_to2_blood
+        self._modelEngine.Models[self.CompBlood].Solutes["Tco2"] = new_tco2_blood
+        self._modelEngine.Models[self.CompGas].Co2 = new_co2_gas
+        self._modelEngine.Models[self.CompGas].Cco2 = new_cco2_gas
